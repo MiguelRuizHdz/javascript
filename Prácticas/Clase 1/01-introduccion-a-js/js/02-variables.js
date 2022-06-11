b984456ffd499c546c029768801a0cacca3dde70
Miguel Angel Ruiz Hernández <miguel.ruizhz@uanl.edu.mx>
2022-06-11 10:39:29 -0500

    Prácticas 1-3
    - Práctica 1: Sintaxis
    - Práctica 2: Variables
    - Práctica 3: Variables DOM

---
+++ Prácticas/Clase 1/01-introduccion-a-js/js/02-variables.js
@@ -0,0 +1,39 @@
+/*=====================================
+            VARIABLES
+=====================================*/
+
+// Variables Numéricas
+let numero = 5;
+numero = "hola";
+console.log("numero:", numero);
+
+// Variables de texto
+var palabra = "palabras";
+console.log("palabra:", palabra);
+
+//Variables Boleanas
+var boleana = true;
+console.log("boleana:", boleana);
+
+// Variables de tipo Arreglo (Array)
+var colores = ["rojo","amarillo"]
+console.log("colores:", colores[0]);
+
+// Variables de tipo objeto (object: propiedad y el valor)
+var jugo = {ingrediente1: "fresa",
+			ingrediente2: "mandarina",
+			ingrediente3: "platano"};
+console.log("jugo:", jugo.ingrediente1);
+
+
+// Variables DOM ('Modelo de Objetos del Documento')
+/*El DOM es la estructura de objetos que genera el navegador cuando se carga un documento y se puede alterar mediante javascript para cambiar dinámicamente los contenidos y aspecto de la página.*/
+var caja = document.querySelector("#caja");
+console.log("caja", caja);
+
+caja.style.width = "200px";
+caja.style.height = "200px";
+caja.style.background = "blue";
+
+var cajas = document.querySelectorAll(".cajas");
+console.log("cajas", cajas);
